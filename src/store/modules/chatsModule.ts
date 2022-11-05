dd5b040a11db24dbf0f98f5e89c4e5bfaab0a6c4
Angel Gonzalez <angel.egb@outlook.com>
2022-11-05 09:50:53 -0400

    Add new messages at the end

--- src/store/modules/chatsModule.ts
+++ src/store/modules/chatsModule.ts
@@ -123,7 +123,7 @@ const module = {
     addChatToEnd(state, data) {
       const index = state.chats.findIndex((chat) => chat._id == data._id);
       if (index == -1) {
-        state.chats.unshift({ ...data, pending_messages_count: 0 });
+        state.chats.push({ ...data, pending_messages_count: 0 });
       }
     },
     deletedMessage(state, data) {
