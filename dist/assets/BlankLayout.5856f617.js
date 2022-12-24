faab9be3cebda739af747db103995bf05857879a
ViktorJJF <vj.jimenez96@gmail.com>
2022-12-24 10:31:20 -0500

    dist

---
+++ dist/assets/BlankLayout.5856f617.js
@@ -0,0 +1,1 @@
+import{m as e,o as a,c as t,w as o,Y as s,a as r,u as c,_ as n}from"./index.55ede2bf.js";const l=e({setup(p){return(u,_)=>(a(),t(s,null,{default:o(()=>[r(c(n))]),_:1}))}});export{l as default};
---
+++ dist/assets/chatSidebar.d4367798.js
@@ -0,0 +1,1 @@
+import{aa as a}from"./index.5cf8c26c.js";const t=a({id:"chatSidebar",state:()=>({sidebarDrawer:!1,bot:null}),getters:{},actions:{SET_SIDEBAR_DRAWER(){this.sidebarDrawer=!this.sidebarDrawer},SET_CURRENT_BOT(e){this.bot=e}}});export{t as u};
