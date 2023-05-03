90ea803e3cd08321c8a3e340d6ba26fcd660f843
ViktorJJF <vj.jimenez96@gmail.com>
2023-05-02 19:23:07 -0500

    build

---
+++ dist/assets/chatSidebar-44a54ad0.js
@@ -0,0 +1,1 @@
+import{aj as r}from"./index-d4393047.js";const a=r({id:"chatSidebar",state:()=>({sidebarDrawer:!1,bot:null}),getters:{},actions:{SET_SIDEBAR_DRAWER(){this.sidebarDrawer=!this.sidebarDrawer},SET_CURRENT_BOT(e){this.bot=e}}});export{a as u};
