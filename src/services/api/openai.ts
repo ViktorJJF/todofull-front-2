f6b931c31aaf16ddc4132e08680602db8627b94d
ViktorJJF <vj.jimenez96@gmail.com>
2023-06-22 20:56:15 -0500

    Update: added openai

---
+++ src/services/api/openai.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import config from "@/config";
+
+export default {
+  generateCompletionForConversation(conversationId) {
+    return axios.post(
+      config.DASHBOARD_BASE_URL + "/api/openai/conversation_completion",
+      { conversationId }
+    );
+  },
+};
