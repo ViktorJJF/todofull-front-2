35241a7206f9b7ef5da7ca6b2c37fba6433dfe9a
ViktorJJF <vj.jimenez96@gmail.com>
2023-11-07 18:15:53 -0500

    build

---
+++ dist/assets/BlankLayout-c1d7add9.js
@@ -0,0 +1,1 @@
+import{e as a,r as e,o as t,c as n,w as s,a as r,u as l,G as _,_ as u}from"./index-dd6f4a47.js";const c=a({__name:"BlankLayout",setup(p){return(f,m)=>{const o=e("v-app");return t(),n(o,null,{default:s(()=>[r(l(_))]),_:1})}}}),i=u(c,[["__file","D:/trabajo/Todofull/todofull-front-2/src/layouts/blank/BlankLayout.vue"]]);export{i as default};
