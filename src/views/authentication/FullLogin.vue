6cc54f274b68d08ac837750fda50b8d1af011219
Cristian Peralta <cbperaltasegura@gmail.com>
2023-11-22 20:25:31 -0500

    feat: Add first template code of new Login

--- src/views/authentication/FullLogin.vue
+++ src/views/authentication/FullLogin.vue
@@ -1,121 +1,99 @@
 <template>
-  <v-container id="login" class="fill-height justify-center" tag="section">
-    <v-row justify="center" class="h-100vh" align="center">
-      <v-col lg="11" sm="8" xl="7">
-        <v-card class="elevation-4">
-          <v-row>
-            <v-col
-              lg="7"
-              class="bg-info d-none d-md-flex align-center justify-center"
-            >
-              <div class="d-none d-sm-block">
-                <div class="d-flex align-center pa-10">
-                  <div>
-                    <h2 class="text-h4 text-white font-weight-medium">
-                      Elegant Design with unlimited features, built with love
-                    </h2>
-                    <h6
-                      class="
-                        text-subtitle-1 text-high-emphasis
-                        mt-4
-                        text-white
-                        font-weight-regular
-                      "
-                    >
-                      Wrappixel helps developers to build organized and
-                      well-coded admin dashboards full of beautiful and feature
-                      rich modules.
-                    </h6>
-                    <v-btn
-                      class="mt-6 text-capitalize"
-                      size="x-large"
-                      variant="outlined"
-                      color="white"
-                      >Learn More</v-btn
-                    >
+  <div class="w-[1440px] h-[900px] px-[135px] pt-[175px] pb-[90px] bg-sky-50 flex-col justify-end items-center gap-10 inline-flex">
+      <div class="self-stretch shadow justify-start items-start inline-flex">
+          <div class="w-[585px] h-[550px] p-2.5 bg-white flex-col justify-center items-center gap-2.5 inline-flex">
+              <div class="flex-col justify-start items-center gap-8 flex">
+                  <div class="w-[72px] h-[72px] justify-center items-center inline-flex">
+                      <img class="w-[72px] h-[72px]" src="@/assets/images/login/icono.png" />
                   </div>
-                </div>
+                  <div class="flex-col justify-start items-center gap-4 flex">
+                      <div class="flex-col justify-start items-start gap-2 flex">
+                          <div class="w-[383px] h-12 px-4 bg-neutral-50 rounded-lg border border-gray-200 justify-between items-center inline-flex">
+                              <div class="text-gray-500 text-sm font-normal font-['Inter'] leading-[18px]">Email</div>
+                              <div class="w-3.5 h-3.5 relative"></div>
+                          </div>
+                      </div>
+                      <div class="flex-col justify-start items-start gap-2 flex">
+                          <div class="w-[383px] h-12 px-4 bg-neutral-50 rounded-lg border border-gray-200 justify-between items-center inline-flex">
+                              <div class="text-gray-500 text-sm font-normal font-['Inter'] leading-[18px]">Contraseña</div>
+                              <div class="w-3.5 h-3.5 relative"></div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="flex-col justify-start items-center gap-4 flex">
+                      <div class="self-stretch h-12 p-2.5 bg-blue-800 rounded-[5px] shadow flex-col justify-center items-center gap-2.5 flex">
+                          <div class="justify-start items-center gap-2 inline-flex">
+                              <div class="text-center text-gray-200 text-sm font-semibold font-['Inter']">Iniciar Sesión</div>
+                          </div>
+                      </div>
+                      <div class="w-[383px] h-12 p-2.5 bg-white rounded-[5px] border border-gray-200 flex-col justify-center items-center gap-2.5 flex">
+                          <div class="self-stretch justify-center items-center gap-2 inline-flex">
+                              <div class="w-4 h-4 relative"></div>
+                              <div class="text-center text-gray-900 text-sm font-normal font-['Inter'] leading-[18px]">Iniciar Sesión con Google</div>
+                          </div>
+                      </div>
+                      <div class="w-[383px] h-12 p-2.5 bg-white rounded-[5px] border border-gray-200 flex-col justify-center items-center gap-2.5 flex">
+                          <div class="self-stretch justify-center items-center gap-2 inline-flex">
+                              <div class="w-4 h-4 relative"></div>
+                              <div class="text-center text-gray-900 text-sm font-normal font-['Inter'] leading-[18px]">Iniciar Sesión con Facebook</div>
+                          </div>
+                      </div>
+                      <div class="text-center text-blue-800 text-sm font-normal font-['Inter'] leading-[18px]">¿Olvidaste tu contraseña?</div>
+                  </div>
               </div>
-            </v-col>
-            <v-col lg="5">
-              <div class="pa-7 pa-sm-12">
-                <img src="@/assets/images/logo-icon.png" />
-                <h2 class="font-weight-bold mt-4 text--darken-2">Sign in</h2>
-                <h6 class="text-subtitle-1 text-grey-darken-1">
-                  Don't have an account?
-                  <a
-                    href="#/pages/fullregister"
-                    class="text-primary text-decoration-none"
-                    >Sign Up</a
-                  >
-                </h6>
-
-                <v-form
-                  ref="form"
-                  v-model="valid"
-                  lazy-validation
-                  action="/dashboards/analytical"
-                >
-                  <v-text-field
-                    v-model="user.email"
-                    :rules="emailRules"
-                    label="E-mail"
-                    class="mt-4"
-                    required
-                    variant="outlined"
-                  ></v-text-field>
-                  <v-text-field
-                    v-model="user.password"
-                    :counter="10"
-                    :rules="passwordRules"
-                    label="Password"
-                    required
-                    variant="outlined"
-                    type="password"
-                  ></v-text-field>
-
-                  <div class="d-block d-sm-flex align-center mb-4 mb-sm-0">
-                    <v-checkbox
-                      v-model="checkbox"
-                      :rules="[(v) => !!v || 'You must agree to continue!']"
-                      label="Remember me?"
-                      required
-                      hide-details
-                    ></v-checkbox>
-                    <div class="ml-auto">
-                      <a
-                        href="javascript:void(0)"
-                        class="text-primary text-decoration-none"
-                        >Forgot pwd?</a
-                      >
-                    </div>
+          </div>
+          <div class="w-[585px] h-[550px] relative">
+              <img class="w-[585px] h-[550px] left-0 top-0 absolute" src="https://via.placeholder.com/585x550" />
+              <div class="w-[585px] h-[550px] p-8 left-0 top-0 absolute bg-slate-800 bg-opacity-95 flex-col justify-end items-start gap-2.5 inline-flex">
+                  <div class="flex-col justify-center items-start flex">
+                      <div class="w-[219px] text-gray-200 text-2xl font-light font-['Inter']">Bienvenidos a</div>
+                      <div class="w-[219px] text-gray-200 text-[32px] font-bold font-['Inter']">TodoFull</div>
                   </div>
-                  <v-btn
-                    @click="login"
-                    color="secondary"
-                    block
-                    class="mr-4"
-                    submit
-                    >Sign In</v-btn
-                  >
-                </v-form>
-                <div class="text-center mt-6">
-                  <div class="d-flex align-center justify-center gap-2">
-                    <v-btn icon color="secondary">
-                      <v-icon>mdi-twitter</v-icon>
-                    </v-btn>
-                    <v-btn icon color="primary">
-                      <v-icon>mdi-github</v-icon>
-                    </v-btn>
-                  </div>
-                </div>
               </div>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+          </div>
+      </div>
+      <div class="self-stretch h-[45px] pb-[21px] justify-center items-start gap-[150px] inline-flex">
+          <div class="self-stretch justify-start items-center gap-4 inline-flex">
+              <div class="justify-start items-end gap-2 flex">
+                  <div class="w-6 h-6 relative">
+                      <div class="w-6 h-6 left-0 top-0 absolute bg-zinc-300"></div>
+                  </div>
+                  <div class="text-blue-950 text-base font-normal font-['Inter']">TodoFull, 123 East, 17th Street, St. louis 10001,</div>
+              </div>
+              <div class="w-[341px] self-stretch justify-start items-end gap-4 flex">
+                  <div class="text-blue-950 text-base font-normal font-['Inter']">Copyright © 2023 • Cigars People Company.</div>
+              </div>
+          </div>
+          <div class="self-stretch flex-col justify-end items-start gap-4 inline-flex">
+              <div class="justify-start items-center gap-[26px] inline-flex">
+                  <div class="justify-start items-start gap-6 flex">
+                      <div class="w-6 h-6 relative">
+                          <div class="w-6 h-6 left-0 top-0 absolute"></div>
+                      </div>
+                      <div class="w-6 h-6 relative">
+                          <div class="w-6 h-6 left-0 top-0 absolute"></div>
+                      </div>
+                      <div class="w-6 h-6 relative">
+                          <div class="w-6 h-6 left-0 top-0 absolute"></div>
+                          <img class="w-[18px] h-4 left-[3px] top-[4px] absolute" src="https://via.placeholder.com/18x16" />
+                      </div>
+                      <div class="w-6 h-6 relative">
+                          <div class="w-6 h-6 left-0 top-0 absolute"></div>
+                          <img class="w-[18px] h-[13px] left-[3px] top-[6px] absolute" src="https://via.placeholder.com/18x13" />
+                      </div>
+                      <div class="w-6 h-6 relative">
+                          <div class="w-6 h-6 left-0 top-0 absolute"></div>
+                          <img class="w-[18px] h-[18px] left-[3px] top-[3px] absolute" src="https://via.placeholder.com/18x18" />
+                      </div>
+                      <div class="w-6 h-6 relative">
+                          <div class="w-6 h-6 left-0 top-0 absolute"></div>
+                          <img class="w-[18px] h-[11px] left-[3px] top-[7px] absolute" src="https://via.placeholder.com/18x11" />
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -150,5 +128,5 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="">
 </style>
