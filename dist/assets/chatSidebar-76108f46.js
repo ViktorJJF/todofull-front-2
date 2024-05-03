13af979204121670d16708690d03aae5269c423c
Cristian Peralta <cbperaltasegura@gmail.com>
2024-05-03 11:18:52 -0500

    chore: update from dev and build

---
+++ dist/assets/chatSidebar-76108f46.js
@@ -0,0 +1,1 @@
+import{am as r}from"./index-c3d8a0a1.js";const a=r({id:"chatSidebar",state:()=>({sidebarDrawer:!1,bot:null}),getters:{},actions:{SET_SIDEBAR_DRAWER(){this.sidebarDrawer=!this.sidebarDrawer},SET_CURRENT_BOT(e){this.bot=e}}});export{a as u};
