7e01c6a68913a22e4eead2ae76a19ebfcf7d7c7b
Cristian Peralta <cbperaltasegura@gmail.com>
2024-05-04 10:56:11 -0500

    feat: Update CHAT_BASE_URL possible values

--- src/config.ts
+++ src/config.ts
@@ -10,6 +10,6 @@ const CHAT_BASE_URL: string =
   environment === "local"
     ? "http://localhost:3031"
     : environment === "development"
-      ? "https://dev.chat-2.todofull.club"
-      : "https://chat-2.todofull.club";
+      ? "https://dev.chat3.todofull.club"
+      : "https://chat3.todofull.club";
 export default { DASHBOARD_BASE_URL, CHAT_BASE_URL };
